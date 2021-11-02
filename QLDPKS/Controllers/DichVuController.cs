2c0bf8a29ba5b96aca42fbfab528e16fbbf6bae1
Pham Thanh Trang <pttrang_19pm@student.agu.edu.vn>
2021-11-02 21:16:56 +0700

    Bảng dịch vụ

---
+++ QLDPKS/Controllers/DichVuController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLDPKS.Models;
+
+namespace QLDPKS.Controllers
+{
+    public class DichVuController : Controller
+    {
+        private QLKSEntities db = new QLKSEntities();
+
+        // GET: Admin/DichVu
+        public ActionResult Index()
+        {
+            return View(db.DichVu.ToList());
+        }
+
+        // GET: Admin/DichVu/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DichVu dichVu = db.DichVu.Find(id);
+            if (dichVu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dichVu);
+        }
+
+        // GET: Admin/DichVu/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/DichVu/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,TenDichVu")] DichVu dichVu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DichVu.Add(dichVu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dichVu);
+        }
+
+        // GET: Admin/DichVu/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DichVu dichVu = db.DichVu.Find(id);
+            if (dichVu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dichVu);
+        }
+
+        // POST: Admin/DichVu/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,TenDichVu")] DichVu dichVu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dichVu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dichVu);
+        }
+
+        // GET: Admin/DichVu/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DichVu dichVu = db.DichVu.Find(id);
+            if (dichVu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dichVu);
+        }
+
+        // POST: Admin/DichVu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DichVu dichVu = db.DichVu.Find(id);
+            db.DichVu.Remove(dichVu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
