e3b4e199b5d8c86c0a322c6939ca2c2f65dd4a3c
PThanhTram <pttram_19pm@student.agu.edu.vn>
2021-11-03 17:34:17 +0700

    Bảng bình luận

---
+++ QLDPKS/Controllers/BinhLuanController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLDPKS.Models;
+
+namespace QLKS.Areas.Admin.Controllers
+{
+    public class BinhLuanController : Controller
+    {
+        private QLKSEntities db = new QLKSEntities();
+
+        // GET: Admin/BinhLuan
+        public ActionResult Index()
+        {
+            var binhLuan = db.BinhLuan.Include(b => b.BaiViet).Include(b => b.KhachHang);
+            return View(binhLuan.ToList());
+        }
+
+        // GET: Admin/BaiViet/Approved/1
+        public ActionResult Approved(int id)
+        {
+            BinhLuan bv = db.BinhLuan.Find(id);
+            bv.KiemDuyet = System.Convert.ToByte(1 - bv.KiemDuyet); // 1 -> 0 và 0 -> 1
+            db.Entry(bv).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: Admin/BinhLuan/Create
+        public ActionResult Create()
+        {
+            ViewBag.BaiViet_ID = new SelectList(db.BaiViet, "ID", "TieuDe");
+            ViewBag.KhachHang_ID = new SelectList(db.KhachHang, "ID", "HoVaTen");
+            return View();
+        }
+
+        // POST: Admin/BinhLuan/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create([Bind(Include = "ID,BaiViet_ID,KhachHang_ID,NoiDung,NgayDang,KiemDuyet")] BinhLuan binhLuan)
+        {
+            if (ModelState.IsValid)
+            {
+                //Bổ sung các trường còn thiếu
+                //binhLuan.KhachHang_ID = Convert.ToInt32(Session["MaKhachHang"]);
+                binhLuan.NgayDang = DateTime.Now;
+                binhLuan.KiemDuyet = binhLuan.KiemDuyet = 0;
+
+                db.BinhLuan.Add(binhLuan);
+                db.SaveChanges();
+                return RedirectToAction("Detail", "Home", new { id = binhLuan.BaiViet_ID, area = "" });
+            }
+
+            ViewBag.BaiViet_ID = new SelectList(db.BaiViet, "ID", "TieuDe", binhLuan.BaiViet_ID);
+            ViewBag.KhachHang_ID = new SelectList(db.KhachHang, "ID", "HoVaTen", binhLuan.KhachHang_ID);
+            return View(binhLuan);
+        }
+
+        // GET: Admin/BinhLuan/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BinhLuan binhLuan = db.BinhLuan.Find(id);
+            if (binhLuan == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BaiViet_ID = new SelectList(db.BaiViet, "ID", "TieuDe", binhLuan.BaiViet_ID);
+            ViewBag.KhachHang_ID = new SelectList(db.KhachHang, "ID", "HoVaTen", binhLuan.KhachHang_ID);
+            return View(binhLuan);
+        }
+
+        // POST: Admin/BinhLuan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit([Bind(Include = "ID,BaiViet_ID,KhachHang_ID,NoiDung,NgayDang,KiemDuyet")] BinhLuan binhLuan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(binhLuan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BaiViet_ID = new SelectList(db.BaiViet, "ID", "TieuDe", binhLuan.BaiViet_ID);
+            ViewBag.KhachHang_ID = new SelectList(db.KhachHang, "ID", "HoVaTen", binhLuan.KhachHang_ID);
+            return View(binhLuan);
+        }
+
+        // GET: Admin/BinhLuan/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BinhLuan binhLuan = db.BinhLuan.Find(id);
+            if (binhLuan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(binhLuan);
+        }
+
+        // POST: Admin/BinhLuan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BinhLuan binhLuan = db.BinhLuan.Find(id);
+            db.BinhLuan.Remove(binhLuan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- QLDPKS/Controllers/HomeController.cs
+++ QLDPKS/Controllers/HomeController.cs
@@ -1,30 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using QLDPKS.Models;
 namespace QLDPKS.Controllers
 {
     public class HomeController : Controller
     {
+        QLKSEntities db = new QLKSEntities();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Details(int id)
         {
-            ViewBag.Message = "Your application description page.";
+            // Lấy thông tin bài viết
+            var baiViet = db.BaiViet.Where(r => r.KiemDuyet == 1 && r.ID == id).SingleOrDefault();
 
-            return View();
+            // Tăng lượt xem
+            // Chính sách: Mỗi máy chỉ tăng 1 lượt xem
+            if (Object.Equals(Session["DaXem" + baiViet.ID], null))
+            {
+                BaiViet bv = db.BaiViet.Find(id);
+                bv.LuotXem = bv.LuotXem + 1;
+                db.Entry(bv).State = EntityState.Modified;
+                db.SaveChanges();
+
+                // Đánh dấu là đã xem
+                Session["DaXem" + bv.ID] = 1;
+            }
+
+            // Hiển thị bài viết ra View
+            return View(baiViet);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
     }
 }
